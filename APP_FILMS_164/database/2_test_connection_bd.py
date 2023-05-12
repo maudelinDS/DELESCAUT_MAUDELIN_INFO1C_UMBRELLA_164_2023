7622723595d4698e4a01f306730b100189b0d678
maudelinDS <maudelin.delescaut@jobtrek.ch>
2023-05-12 21:04:51 +0200

    Fix SQL query for test

--- APP_FILMS_164/database/2_test_connection_bd.py
+++ APP_FILMS_164/database/2_test_connection_bd.py
@@ -9,7 +9,7 @@ try:
     """
         Une seule requête pour montrer la récupération des données de la BD en MySql.
     """
-    strsql_genres_afficher = """SELECT id_genre, intitule_genre, date_ins_genre FROM t_genre ORDER BY id_genre ASC"""
+    strsql_genres_afficher = """SELECT  activity_id,activity_name  FROM t_activity ORDER BY activity_id ASC"""
 
     with DBconnection() as db:
         db.execute(strsql_genres_afficher)
